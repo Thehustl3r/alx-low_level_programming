5372883d90e168e41790c86c04d71474704e2650
prosper <mprosp3r@gmail.com>
2023-03-17 06:22:15 -0700

    1,2,4,5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void){
-	char myString[]= '/"Programming is like building a multilingual puzzle \n';
+	char myString[]= "Programming is like building a multilingual puzzle \n";
 	puts(myString);
 	return 0;
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void){
 	char mystring[]= "with proper grammar, but the outcome is a piece of art,\n";
-	puts(mystring);
+	printf("%s \n"mystring);
 	return 0;
 }
