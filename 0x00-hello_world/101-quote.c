929e9f16a2d3131ba97b3dac27721efcd037fa1f
prosper <mprosp3r@gmail.com>
2023-03-17 13:54:55 -0700

    8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful \" - Dora Korpar, 2015-10-19\n");
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
