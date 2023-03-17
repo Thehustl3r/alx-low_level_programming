366bdf3784cedbe06416ea76887ef8627b465962
prosper <mprosp3r@gmail.com>
2023-03-17 16:03:19 -0700

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,10 +22,10 @@ int main(void)
 		printf("Last digit of %d is %d and is greater than 5\n", n, lastdigit);
 	} else if (lastdigit == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n",n, lastdigit);
+		printf("Last digit of %d is %d and is 0\n", n, lastdigit);
 	} else if (lastdigit < 6 && lastdigit != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n" , n, lastdigit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
 	}
 	return (0);
 }
