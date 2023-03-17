0772cebf0e2fecb090e079087f80ea8b8960c552
prosper <mprosp3r@gmail.com>
2023-03-17 15:20:59 -0700

    0,1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,11 +10,16 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n < 0){
+
+	if (n < 0)
+	{
 		printf("%d is negative", n);
-	}else if(n > 0){
+	}else if (n > 0)
+	{
 		printf("%d is positice", n);
-	}else{
+	}else
+	{
 		printf("is zero");
+	}
 	return (0);
 }
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,12 +12,15 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	lastdigit = n % 10;
-	if(lastdigit > 5){
-		printf("Last digit of %d is %d and is greater than 5", n , lastdigit);
-	}else if(lastdigit == 0){
-		printf("Last digit of %d is %d is 0", n , lastdigit);
-	}else if (lastdigit < 6 && lastdigit != 0){
-		printf("Last digit of %d is %d and is less than 6 and not 0", n , lastdigit);
+	if (lastdigit > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5 ", n , lastdigit);
+	}else if (lastdigit == 0)
+	{
+		printf("Last digit of %d is %d and is 0 ", n , lastdigit);
+	}else if (lastdigit < 6 && lastdigit != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0 ", n , lastdigit);
 	}
 	return (0);
 }
