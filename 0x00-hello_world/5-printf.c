8a3ecd2e49690190c77481a2798a273abdec143d
prosper <mprosp3r@gmail.com>
2023-03-17 06:24:31 -0700

    1,2,4,5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void){
 	char mystring[]= "with proper grammar, but the outcome is a piece of art,\n";
-	printf("%s \n"mystring);
+	printf("%s\n",mystring);
 	return 0;
 }
