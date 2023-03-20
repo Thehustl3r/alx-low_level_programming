77ac7f1f6eface61885655ff00c4b666fb80dc3d
prosper <mprosp3r@gmail.com>
2023-03-20 05:01:43 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,17 +2,17 @@
 
 /**
  * main - Entry function
- * return (0) - success
+ * Return: always returb 0 (success)
  */
 int main(void)
 {
 	int i;
-	for (i = 1; i <= 89; i++)
+	for (i = '1'; i <= '89'; i++)
 	{
 		if (i < 10)
-			putchar(i == 1 ? ('0' + i): (' ' + '0' + i));
+			putchar(i == 1 ? ('0' + i + ',') : (' ' + '0' + i + ','));
 		else
-			putchar(' ' + i);
+			putchar(' ' + i + ',');
 	}
 	putchar('\n');
 	return (0);
