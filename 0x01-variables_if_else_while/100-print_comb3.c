189a24faf2999da7ded1bf252d67be058da328ed
prosper <mprosp3r@gmail.com>
2023-03-20 04:50:09 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry function
+ * return (0) - success
+ */
+int main(void)
+{
+	int i;
+	for (i = 1; i <= 89; i++)
+	{
+		if (i < 10)
+			putchar(i == 1 ? ('0' + i): (' 0' + i));
+		else
+			putchar(' ' + i);
+	}
+	putchar('\n');
+	return (0);
+}
