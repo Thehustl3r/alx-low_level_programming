6dc0cd0a04578d39555bbc77d00cd25407276388
prosper <mprosp3r@gmail.com>
2023-03-21 14:39:55 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - print all alphabet
+ * Return: always 0 (success)
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+
+	_putchar('\n');
+	return (0);
+}
