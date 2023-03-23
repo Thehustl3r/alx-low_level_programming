5e4dec7da6918b81f060a8d683fe0674309d7ae3
prosper <mprosp3r@gmail.com>
2023-03-23 09:58:58 -0700

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,12 +7,13 @@
  */
 void print_most_numbers(void)
 {
-	int i; 
+	int i = '0';
 
-	for (i = '0'; i <= '9'; i++)
+	for (; i <= '9'; i++)
 	{
 		if ( i == '2' || i == '4')
 			continue;
+
 		_putchar(i);
 	}
 	_putchar('\n');
