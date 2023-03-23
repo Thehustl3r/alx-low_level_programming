2bcab1f59caea3fb75bb5731b6a6c55d98682152
prosper <mprosp3r@gmail.com>
2023-03-23 09:13:59 -0700

    1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,25 +1,23 @@
 #include "main.h"
 
 /**
- * main: - entry program
- * _isdigit: - prgram to verify if is int
- * @c: - An integer c
+ * _isdigit - A function that checks for a digit (0 through 9).
+ * @c: An input integer
  * Return: 1 if c is a digit or 0 otherwise
  */
-
 int _isdigit(int c)
 {
-	char i;
-	int flag = 0;
+	char i = '0';
+	int isdigit = 0;
 
-	for (i = '0'; i <= '9'; i++)
+	for (; i <= '9'; i++)
 	{
 		if (i == c)
 		{
-			flag = 1;
+			isdigit = 1;
 			break;
 		}
 	}
 
-	return (flag);
+	return (isdigit);
 }
