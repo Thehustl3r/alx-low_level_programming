184e98586fe6b3d996e7f753f141349ee658d3eb
prosper <mprosp3r@gmail.com>
2023-03-23 10:21:22 -0700

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,17 +7,18 @@
  */
 void more_numbers(void)
 {
-	int i = 0;
+	int i;
 	int count = 0;
 
 	while (count < 10)
 	{
-		for (; i <= 14; i++)
+		for (i = 0 ; i <= 14; i++)
 		{
 			if (i > 9)
 				_putchar(i / 10 + '0');
 
 			_putchar(i % 10 + '0');
+		}
 		_putchar('\n');
 		count ++;
 	}
