1ac242e34018cc9630dd2fd921983eadc189150e
prosper <mprosp3r@gmail.com>
2023-03-23 09:31:06 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that print num 0 to 9
+ */
+void print_numbers(void)
+{
+	int i;
+	for (i = 48; i <= 58; i++)
+		_putchar(i);
+	_putchar('\n');
+}
