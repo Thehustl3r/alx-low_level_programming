218318d1097f930dd9dceeabd1a345d1fe015596
prosper <mprosp3r@gmail.com>
2023-03-24 03:42:41 -0700

    resolved

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+/**
+ * find_largest_prime_factor - Entry function
+ * @number: An integer number
+ * Return: largest prime number always
+ */
+int find_largest_prime_factor(int number);
+
+/**
+ * main - Entry function
+ * Return: 0 always
+ */
+int main(void)
+{
+	int number;
+
+	number = 1231952;
+
+	printf("%d \n", find_largest_prime_factor(number));
+
+	return (0);
+}
+
+/**
+ * find_largest_prime_factor - Entry function
+ * @number: An integer number
+ * Return: largest prime number always
+ */
+int find_largest_prime_factor(int number)
+{
+	int i = 2;
+	int largest = 0;
+	int j = 2;
+
+	while (i <= number)
+	{
+		if (number % i == 0)
+		{
+			number = number / i;
+
+			for (; j <= i; j++)
+			{
+				if ((i % (j - 1) != 0))
+				{
+					if (i >  largest)
+						largest = i;
+				}
+			}
+			i = 2;
+		}
+		i++;
+	}
+	return (largest);
+}
