34714aa7c0ba35522452a6412396bd3473089806
prosper <mprosp3r@gmail.com>
2023-03-28 15:39:42 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,12 +10,10 @@ void puts_half(char *str)
 {
 	int len = 0;
 	int mid, lenn;
-	bool isOdd = false;
 
 	while (*(str + len) != '\0')
 		len++;
 	mid = len / 2;
-	isOdd = (len % 2) == 0;
 	len--;
 	lenn = len;
 	len = 0;
@@ -25,7 +23,7 @@ void puts_half(char *str)
 		_putchar(*(str + len));
 		len++;
 	}
-	if(isodd)
+	if (((lenn + 1) % 2 != 0))
 		_putchar(*(str + lenn));
 	_putchar('\n');
 }
