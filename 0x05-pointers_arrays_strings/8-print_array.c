8919080e53c90ba6fa884f7c2d698f7e3ca8844a
prosper <mprosp3r@gmail.com>
2023-03-28 16:05:53 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,44 @@
+#include "main.h"
+int print(int n);
+/**
+ * print_array - Function to print array
+ * @a: Pointer a
+ * @n: integer n
+ *
+ * Return: Nothing
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while ( i < n)
+	{
+		print(*(a + i));
+
+		if ( i != (n - 1))
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+}
+
+/**
+ * print - print number
+ * @n: Integer a
+ *
+ * Return: number
+ */
+int print(int n)
+{
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+	if (n / 10)
+		print(n /10);
+	_putchar(n + '0');
+}
