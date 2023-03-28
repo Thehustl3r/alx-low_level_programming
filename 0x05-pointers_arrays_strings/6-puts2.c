906848b44a6648c048ad5a49bf014793fa14be5b
prosper <mprosp3r@gmail.com>
2023-03-28 15:04:32 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts2 - print string followed by new line
+ * @str: char str
+ *
+ * Return: Nothing
+ */
+
+void puts2(char *str)
+{
+	int len = 0;
+
+	while (*(str + len) != '\0')
+		len++;
+	len--;
+
+	while (len >= 0)
+	{
+		_putchar(*(str + len));
+		_putchar('\n');
+		len--;
+	}
+}
