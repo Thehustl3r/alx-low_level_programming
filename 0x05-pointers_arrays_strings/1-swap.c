caf59a80f1634e35d281374e075c6b58b0269d1e
prosper <mprosp3r@gmail.com>
2023-03-28 10:06:39 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * swap_int - the function that swap two numbers
+ * @a: pointer a
+ * @b: pointer b
+ *
+ * Return: Nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *tmp;
+
+	*tmp = *a;
+	*a = *b;
+	*b = *tmp;
+}
