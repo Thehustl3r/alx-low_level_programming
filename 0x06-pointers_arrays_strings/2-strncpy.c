25c23a5463103bbb2d2562fd764a8eabe41c9999
prosper <mprosp3r@gmail.com>
2023-03-29 09:27:54 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * *_strncpy - function that copy the value at index
+ * @dest: a character dest
+ * @src: a character src
+ * @n: an integer n
+ *
+ * Return: copy 
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int len1, len2, total, i;
+
+	len1 = 0;
+	len2 = 0;
+	i = 0;
+
+	while (*(dest + len1) != '\0')
+		len1++;
+	while (*(src + len2) != '\0')
+		len2++;
+	total = (len2 >= n) ? n : len2;
+	total++;
+
+	while (i < total)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
