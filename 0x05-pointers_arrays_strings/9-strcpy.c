e51f25a0bbd0a8ab17e3e098c3655563fff402f8
prosper <mprosp3r@gmail.com>
2023-03-29 05:34:27 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,17 +8,13 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int len1, len2, total, i;
+	int len2, i;
 
-	len1 = 0;
 	len2 = 0;
 	i = 0;
 
-	while (*(dest + len1) != '\0')
-		len1++;
 	while (*(src + len2) != '\0')
 		len2++;
-	total = len1 + len2;
 
 	while (i < len2)
 	{
