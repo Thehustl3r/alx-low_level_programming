0a57bb907da8faf8fb945d9b40ad1f78abb33062
prosper <mprosp3r@gmail.com>
2023-03-30 13:36:33 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -18,13 +18,13 @@ char *leet(char *str)
 	{
 		if (str[i] == 'a' || str[i] == 'A')
 			str[i] = '4';
-		if (str[i] == 'e' || str[i] == 'E')
+		else if (str[i] == 'e' || str[i] == 'E')
 			str[i] = '3';
-		if (str[i] == 'o' || str[i] == 'O')
+		else if (str[i] == 'o' || str[i] == 'O')
 			str[i] = '0';
-		if (str[i] == 't' || str[i] == 'T')
+		else if (str[i] == 't' || str[i] == 'T')
 			str[i] = '7';
-		if (str[i] == 'l' || str[i] == 'L')
+		else if (str[i] == 'l' || str[i] == 'L')
 			str[i] = '1';
 		i++;
 	}
