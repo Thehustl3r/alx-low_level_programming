b00a414d568632b8f97cc2d181e1598aaa152667
prosper <mprosp3r@gmail.com>
2023-03-29 19:41:55 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * *rot13 - convert to uppercase
+ * @str: a char pointer
+ *
+ * Return: character
+ */
+char *rot13(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (*(str + len) != '\0')
+	{
+		while ((str[i] > 64 && str[i] < 91) || (
+				str[i] > 96 && str[i] < 123))
+		{
+			if ((*(str + i) > 64 && *(str + i) < 91 && *(str + i) < 78 ) || (*(str + i) > 96 && *(str + i) < 123  && *(str + i) < 110) )
+			{
+				str[i] += 13;
+			}
+			else 
+				str[i] -= 13;
+			i++;
+		}
+		i++;
+	}
+	return (str);
+}
