ef67d576bfb092e5dbc3b2aca27742102b72f15b
prosper <mprosp3r@gmail.com>
2023-03-31 04:16:43 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -17,9 +17,7 @@ char *rot13(char *str)
 				str[i] > 96 && str[i] < 123))
 		{
 			if ((*(str + i) > 64 && *(str + i) < 91 && *(str + i) < 78 ) || (*(str + i) > 96 && *(str + i) < 123  && *(str + i) < 110) )
-			{
 				str[i] += 13;
-			}
 			else 
 				str[i] -= 13;
 			i++;
