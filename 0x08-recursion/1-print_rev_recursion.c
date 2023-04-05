440edd573fba5f72e8c7bd10081557983398a1a8
prosper <mprosp3r@gmail.com>
2023-04-05 08:05:40 -0700

    1-print_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - print reverse of the string
+ * @s: point to the address of the string
+ *
+ * Return: Nothing
+ */
+
+_print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(s);
+	s++;
+}
