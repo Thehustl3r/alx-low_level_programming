68f411c2aaf6d6b4c24a1310a2b901980effecf0
prosper <mprosp3r@gmail.com>
2023-04-05 08:53:10 -0700

    3-factorial.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strlen_recursion = the function that orints the klength
+ * _strlen_recursion - the function that orints the klength
  * @s: a character s
  *
  * Return: length
@@ -12,6 +12,7 @@ int _strlen_recursion(char *s)
 
 	if (*s == '\0')
 		return(0);
+
 	_strlen_recursion(s++);
 	return(i + 1);
 }
---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * factorial - the function that prints the factorial
+ * @n: an integer n
+ *
+ * Return: factorial
+ */
+
+int factorial(int n)
+{
+	if ( n < 0)
+		return(-1);
+	if (n == 0)
+		return (0);
+	return(n * factorial(n - 1));
+}
