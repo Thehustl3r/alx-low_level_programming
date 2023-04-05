60ae77aad1ccf5c89c962ffebec43d67473229fa
prosper <mprosp3r@gmail.com>
2023-04-05 03:02:15 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts_recursion - Function that prints the string with new line
+ * @s: charater s
+ *
+ * Return: Nothing
+ */
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (i < 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	_puts_recursion(s[(i - 1)]);
+	_putchar(a[i]);
+}
