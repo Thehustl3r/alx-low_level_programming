244a37051c13796f4b9ad87d69b188c80c79c09d
prosper <mprosp3r@gmail.com>
2023-04-05 08:34:58 -0700

    _strle_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion = the function that orints the klength
+ * @s: a character s
+ *
+ * Return: length
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s == '\0')
+		return;
+	_strlen_recursion(s++);
+	return (i + 1);
+}
