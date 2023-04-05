a522bc09f3223270a80774a661e6cdf33494bd77
prosper <mprosp3r@gmail.com>
2023-04-05 10:28:45 -0700

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * is_print_number - function to print prime number
+ * @n: an integer n
+ *
+ * Return: 1 if prime orn 0 if not
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 0)
+		return (0);
+	else
+		return (1);
+}
