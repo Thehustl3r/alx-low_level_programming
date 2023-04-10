16c369df294c8863a2f7c1a044d082ca80c55415
prosper <mprosp3r@gmail.com>
2023-04-10 10:16:40 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - Entry function to program
+ * @argc: number of agruments
+ * @argv: number of the agrument
+ *
+ * Return: 0 always
+ */
+
+int main(int argc, char *argv[])
+{
+	int count;
+	if (argc < 2)
+		printf("%s\n", "0");
+	else
+	{
+		for (count = 0; count < argc; count++)
+		{
+			if (!isdigit(argv[count]))
+			{
+				printf("%s\n", "Error");
+				return (1);
+			}
+		}
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
+	return (0);
+}
