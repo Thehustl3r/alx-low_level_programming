ac032aae23601b3416808ed0b475567b7181e1a7
prosper <mprosp3r@gmail.com>
2023-04-10 09:28:54 -0700

    correction

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 /**
  * main - Entry function to program 
- * @argc: number of agruments
+ * @argc: number of agrumentsi
  * @argv: number of the agrument
  *
  * Return: 0 always
  */
 int main(int argc, char *argv[])
 {
+	if (argc < 0)
+		printf("%s", argv[0]);
 	printf("%d\n",argc);
 	return (0);
 }
