8b6bc11e33240430da12ea27f8c7a0d42da3f3d6
prosper <mprosp3r@gmail.com>
2023-04-10 08:55:48 -0700

    1

---
+++ 0x09-static_libraries/0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry function to program 
+ * @argc: number of agruments
+ * @argv: number of the agrument
+ *
+ * Return: 0 always
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n",argv[0]);
+	return (0);
+}
