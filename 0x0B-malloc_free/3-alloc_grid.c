9062917d7b02b0a8cabafe8370d75a16530b247e
prosper <mprosp3r@gmail.com>
2023-04-12 16:41:58 -0700

    3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * **allloc_grid - the function to print two dimension array
+ * @width: value of width
+ * @height: value of length
+ *
+ * Return: 2 dimension arrray
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i = 0;
+	int *grid;
+
+	if (width == NULL || height == NULL)
+		return (0);
+	grid = (int *)malloc((width * height) * sizeof(int));
+
+	for (; i < (height * width); i++)
+		grid[i] = 0;
+
+	return (&grid);
+}
