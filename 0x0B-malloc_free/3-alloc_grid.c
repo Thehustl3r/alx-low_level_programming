c9cd08cb8d401b467e5d5148d1a03b4ae465a528
prosper <mprosp3r@gmail.com>
2023-04-12 16:46:47 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,12 +13,12 @@ int **alloc_grid(int width, int height)
 	int i = 0;
 	int *grid;
 
-	if (width == NULL || height == NULL)
+	if (width == 0 || height == 0)
 		return (0);
 	grid = (int *)malloc((width * height) * sizeof(int));
 
 	for (; i < (height * width); i++)
 		grid[i] = 0;
 
-	return (&grid);
+	return (grid);
 }
