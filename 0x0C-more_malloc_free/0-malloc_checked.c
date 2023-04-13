4556372ac65331c17fd3b49e3e8f1d72ebffc27f
prosper <mprosp3r@gmail.com>
2023-04-13 06:39:10 -0700

    correction

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * *malloc_checked - The function that allocate memory using malloc
+ * @b: unsigned integer b
+ *
+ * Return: Nothing
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	malloc(b);
+}
