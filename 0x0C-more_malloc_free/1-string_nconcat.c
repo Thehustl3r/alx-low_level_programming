87c1416983b1fc01813532a0cd9d0c70e119c1ce
prosper <mprosp3r@gmail.com>
2023-04-13 08:45:39 -0700

    correction

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,25 +17,35 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i = 0;
 	unsigned int total;
 
+	if (s1 == NULL)
+		s1 = "";
+
 	while (s1[len1] != '\0')
 		len1++;
+
+	if (s2 == NULL)
+		s2 = "";
+
 	while (s1[len2] != '\0')
 		len2++;
+
 	if (n < len2)
 		total = len1 + n;
 	else
 		total = len1 + len2;
+	total++;
 	new = malloc(total);
 
-	if (s1 == NULL && s2 == NULL)
+	if (new == NULL)
 		return (0);
 
-	for (; i <= total; i++)
+	for (; i < total; i++)
 	{
 		if (i < len1)
 			new[i] = s1[i];
 		else
 			new[i] = s2[i - len1];
 	}
+	new[i] = '\0';
 	return (new);
 }
