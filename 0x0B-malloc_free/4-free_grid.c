013fb7ef397e85d9105f130ca661b15b885bea29
prosper <mprosp3r@gmail.com>
2023-04-12 17:09:24 -0700

    4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,11 +1,11 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * free_grid - the function to free 
+ * free_grid - the function to free
  * @grid: pointer of pointer
  * @height: integer height
  *
- * Return: 
+ * Return: Nothing
  */
 
 void free_grid(int **grid, int height)
