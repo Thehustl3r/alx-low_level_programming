9cafc53ce90ae48ed315f4d57f45013126bd314a
prosper <mprosp3r@gmail.com>
2023-04-14 06:52:08 -0700

    3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,9 +14,10 @@ int *array_range(int min, int max)
 	if (min < max)
 		return (NULL);
 
-	arr = malloc(sizeof(int *) * (max - min + 1));
+	arr = malloc(sizeof(int) * (max - min + 1));
 	if (arr == NULL)
 		return (NULL);
+
 	for (; min <= max; min++)
 		*arr = min, arr++;
 	return (arr);
