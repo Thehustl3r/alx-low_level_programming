6a65a805c248b919d74958a30258b1379e663bb4
prosper <mprosp3r@gmail.com>
2023-04-14 05:15:15 -0700

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * *_calloc - The functuin that create malloc for string
+ * @nmemb: the integer nmemb
+ * @size: the integer size
+ *
+ * Return: Nothing
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *str;
+	unsigned int i = 0;
+
+	if (nmemb == 0 || size == 0)
+		return (0);
+	str = malloc((nmemb * size) + 1);
+
+	if (str == NULL)
+		return (0);
+
+	for(; i < (nmemb * size); i++)
+		str[i] = "0";
+
+	return (str);
+}
