2f48d3ae817c9149ed9371ee1877ebe91d1bcc5b
prosper <mprosp3r@gmail.com>
2023-04-14 07:59:14 -0700

    correction

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,15 +10,18 @@
 int *array_range(int min, int max)
 {
 	int *arr;
+	int diff;
+	int i = 0;
 
 	if (min > max)
 		return (NULL);
+	diff = max - min;
 
-	arr = malloc(sizeof(int) * (max - min + 1));
+	arr = malloc((diff + 1) * sizeof(int));
 	if (arr == NULL)
 		return (NULL);
 
-	for (; min <= max; min++)
+	for (; i <= diff; i++)
 		*arr = min, arr++;
 	return (arr);
 }
