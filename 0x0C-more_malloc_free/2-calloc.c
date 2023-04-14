661ada932da672e918de5deb61b59785c1bf61f9
prosper <mprosp3r@gmail.com>
2023-04-14 05:43:37 -0700

    correction

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,8 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (0);
 	str = malloc(nmemb * size);
-
-	if (str = NULL)
+	if (str == NULL)
 		retutn (0);
 
 	for (; i < (nmemb * size); i++)
