9a0ef4d83eeb11a8feacdf6ef3f3b2db5b11e277
prosper <mprosp3r@gmail.com>
2023-04-18 08:13:38 -0700

    correction

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,22 +1,31 @@
 #include <stdlib.h>
+/**
+ * struct dog - the struct contains detail of dog
+ * @name: the adrress of name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 }doggy;
-
+/**
+ * struct new_dog - the struct contains detail of dog
+ * @name: the adrress of name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
 struct dog *new_dog(char *name, float age, char *owner)
 {
 	struct dog *dogi;
 
 	dogi = malloc(sizeof(struct dog));
-	
 	if (dogi == NULL)
 		return (NULL);
 	dogi->name = name;
 	dogi->age = age;
 	dogi->owner = owner;
-
 	return (dogi);
 }
