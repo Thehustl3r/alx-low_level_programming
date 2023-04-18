fc7c00af72e9601c69b9464a1deed003aa4bb832
prosper <mprosp3r@gmail.com>
2023-04-18 08:30:54 -0700

    correction

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#include "main.h"
+#include "dog.h"
 /**
  * init_dog - the function that initialize a struct
  * d: thea ddress of the structure
@@ -9,12 +9,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(struct dog));
-
 	if (d == NULL)
 		return (NULL);
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
-	return (NULL);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,31 +1,21 @@
-#include <stdlib.h>
+#ifndef DOG
+#define DOG
+
 /**
- * struct dog - the struct contains detail of dog
- * @name: the adrress of name
+ * struct dog - structure for a dog
+ * @name: dog's name
  * @age: dog's age
  * @owner: dog's owner
  */
+
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 }doggy;
-/**
- * struct new_dog - the struct contains detail of dog
- * @name: the adrress of name
- * @age: dog's age
- * @owner: dog's owner
- */
-struct dog *new_dog(char *name, float age, char *owner)
-{
-	struct dog *dogi;
-
-	dogi = malloc(sizeof(struct dog));
-	if (dogi == NULL)
-		return (NULL);
-	dogi->name = name;
-	dogi->age = age;
-	dogi->owner = owner;
-	return (dogi);
-}
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
