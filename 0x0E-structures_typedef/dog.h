f0323a0b9d9cf09410064d3b3468f2dadf91b5a3
prosper <mprosp3r@gmail.com>
2023-04-18 08:45:42 -0700

    correction

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef _DOG_H_
-#define _DOG_H_
+#ifndef DOG
+#define DOG
 
 /**
  * struct dog - structure for a dog
@@ -13,7 +13,7 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
