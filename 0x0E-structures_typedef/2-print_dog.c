5550a8d30c1022a15d5dc0b0f7a2e8fc2f3885bb
prosper <mprosp3r@gmail.com>
2023-04-18 11:07:08 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 	if (d->owner == NULL)
 		d->owner = "(nil)";
 	printf("Name: %s\n", d->name);
-	printf("Age: %d\n", d->age);
+	printf("Age: %0.6f\n", d->age);
 	printf("Owner: %s\n", d->owner);
 		
 }
