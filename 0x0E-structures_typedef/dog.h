6ba331b3a082794c2450791848c6c2008ac30bed
prosper <mprosp3r@gmail.com>
2023-04-17 17:15:26 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,22 @@
+#include <stdlib.h>
 typedef struct dog
 {
 	char *name;
-	float name;
+	float age;
 	char *owner;
-}dog;
+}doggy;
+
+struct dog *new_dog(char *name, float age, char *owner)
+{
+	struct dog *dogi;
+
+	dogi = malloc(sizeof(struct dog));
+	
+	if (dogi == NULL)
+		return (NULL);
+	dogi->name = name;
+	dogi->age = age;
+	dogi->owner = owner;
+
+	return (dogi);
+}
