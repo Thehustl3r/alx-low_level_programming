b8c17ec990106a066eae54938430f0bf7de734b6
prosper <mprosp3r@gmail.com>
2023-04-21 04:40:32 -0700

    correction

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,9 +16,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(pi, n);
 	for (; i < n; i++)
 	{
-		if (separator == NULL && i != (n - 1))
+		if (separator == NULL)
 			return;
-		
+
 		printf("%d", va_arg(pi, unsigned int));
 		if (separator != NULL && i != (n - 1))
 			printf("%c ", separator[0]);
