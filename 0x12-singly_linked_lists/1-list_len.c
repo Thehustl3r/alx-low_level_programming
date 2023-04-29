91341522607cfdd17a39ffb5ec14452093f8fe8d
prosper <mprosp3r@gmail.com>
2023-04-28 17:32:11 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - A function that gets the number of node of linked list
+ * @h: head of linked list
+ * Return: numbr of nodes as size_t
+ */
+size_t list_len(const list_t *h)
+{
+	unsigned int nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
