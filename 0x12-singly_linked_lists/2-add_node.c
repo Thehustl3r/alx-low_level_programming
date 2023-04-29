5a5c7f9b0013db9be1197fceb1247ec3ffff6d48
prosper <mprosp3r@gmail.com>
2023-04-28 17:34:36 -0700

    2-add_more.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <string.h>
+/**
+ * add_node - A function that adds a node to the head of linked list
+ * @head: pointer to a list_t ponter that point to the head struct
+ * @str: pointer to string that adds as node
+ * Return: pointer to new head of list, NULL on filure
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *temp;
+	int length = 0;
+
+	temp = malloc(sizeof(list_t));
+	if (temp == NULL)
+		return (NULL);
+
+	while (str[length])
+		length++;
+
+	temp->len = length;
+	temp->str = strdup(str);
+	temp->next = *head;
+	*head = temp;
+	return (temp);
+}
