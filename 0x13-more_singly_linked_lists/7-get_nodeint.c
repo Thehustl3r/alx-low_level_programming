33dde12078c7c074ccbeffe127b4f94f8fb9bdd8
prosper <mprosp3r@gmail.com>
2023-05-01 09:24:58 -0700

    correction

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -5,6 +5,7 @@
  * get_nodeint_at_index - the function that return the index at index
  * @head: the pointer to head
  * @index: the index
+ * Return: new node
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
