09cd243d79137e4e3b787a6d0ae4cfcbd8fe3933
prosper <mprosp3r@gmail.com>
2023-05-01 03:45:10 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	while (head != NULL)
 	{
-		if (idx == idx)
+		if (idx == index)
 		{
 			new_node = malloc(sizeof(listint_t));
 			if (new_node == NULL)
@@ -28,7 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (new_node);
 
 		}
-		head = head->next;
+		(*head) = (*head)->next;
 		index++;
 	}
 	return (NULL);
