cd00e3791e5bf30c1f5169fc5ec3700ad960d20b
prosper <mprosp3r@gmail.com>
2023-05-01 14:38:15 -0700

    correction

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,7 +4,7 @@
 
 /**
  * print_listint - the function that prints number of node in the list
- * @h: pointer to the heard
+ * @hi: pointer to the heard
  * Return: number of node
  */
 size_t print_listint(const listint_t *h)
