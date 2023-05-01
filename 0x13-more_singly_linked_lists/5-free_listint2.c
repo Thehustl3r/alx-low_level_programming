508a17ddff42a3857261809349b55c8f06f965da
prosper <mprosp3r@gmail.com>
2023-04-30 23:01:48 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - the function that frees the memory
+ * @head: pointer to the head
+ * Return: Nothing
+ */
+void free_listint2(listint_t **head)
+{
+	if (head == NULL)
+		return;
+	free(*head);
+}
