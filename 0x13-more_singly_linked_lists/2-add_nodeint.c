2761ad3f5982345eddfe4b720ac83bf8189e2322
prosper <mprosp3r@gmail.com>
2023-04-30 22:03:39 -0700

    correction

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,7 +11,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_node;
 
-	new_node = malloc(sizeof(add_nodeint));
+	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
