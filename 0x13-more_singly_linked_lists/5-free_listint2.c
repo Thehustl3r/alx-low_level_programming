52693feedaee1175f059e436668599ab1459212b
prosper <mprosp3r@gmail.com>
2023-05-01 09:21:35 -0700

    correction

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,7 +8,15 @@
  */
 void free_listint2(listint_t **head)
 {
+	listint_t *tmp;
+
 	if (head == NULL)
 		return;
-	free(*head);
+	while (*head)
+	{
+		tmp = *head;
+		(*head) = (*head)->next;
+		free(tmp);
+	}
+	*head = NULL;
 }
