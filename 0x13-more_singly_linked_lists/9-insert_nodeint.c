358ba938813ff628420934fdb46d0c8e41b4efb7
prosper <mprosp3r@gmail.com>
2023-05-01 09:37:49 -0700

    correctio

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,11 +26,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			new_node->n = n;
 			new_node->next = (*head)->next;
 			(*head)->next = new_node;
-			return (new_node);
-
 		}
 		(*head) = (*head)->next;
 		index++;
 	}
-	return (NULL);
+	return (new_node);
 }
