533b95e48677cf253b6c102a1fbec70058297b1b
prosper <mprosp3r@gmail.com>
2023-05-03 02:24:47 -0700

    correction on lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -4,7 +4,7 @@
 /**
  * struct listint_s - singly linked list
  * @n: integer
- * @next: points to the next node 
+ * @next: points to the next node
  *
  * Description: singly linked list node structure
  *
