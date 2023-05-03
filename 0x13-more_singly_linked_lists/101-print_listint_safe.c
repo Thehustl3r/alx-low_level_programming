26f780cce3dd1824edf3fd36f224ef5914f0ef82
prosper <mprosp3r@gmail.com>
2023-05-03 04:15:48 -0700

    correction

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,19 +10,27 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
+	const listint_t *tmp1 = head, *tmp2 = head;
 	size_t i = 0;
 
 	if (head == NULL)
 	{
 		exit (98);
 	}
-	while (head !=  NULL)
+	while (head && tmp1 && tmp2 && tmp2->next)
 	{
+		tmp1 = tmp1->next;
+		tmp2 = tmp2->next->next;
+
+		if (tmp1 == tmp2)
+		{
+			printf("-> [%p] %d\n",(void *)head, head->n);
+			exit (98);
+		}
 		printf("[%p] %d\n",(void *)head, head->n);
 		head = head->next;
 		i++;
-		if (head == NULL)
-			break;
 	}
+	head = NULL;
 	return (i);
 }
