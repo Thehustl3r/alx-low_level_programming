3f17f24df6a6b37ede622b93e99781ed7b9ce619
prosper <mprosp3r@gmail.com>
2023-05-05 08:08:39 -0700

    2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * get_bit - function that returns the bit at index
+ * @n: the value
+ * @index: Index
+ * Return: bit if its found otherwise -1
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int ne = (int)n;
+	unsigned int idx = 0, rem, test;
+
+	while (ne > 0)
+	{
+		rem = ne % 2;
+		ne = ne / 2;
+		idx++;
+	}
+	ne = (int)n;
+	test = idx;
+	while (ne > 0)
+	{
+		rem = ne % 2;
+		if (index == (test - idx))
+			return (rem);
+		ne = ne / 2;
+		idx--;
+	}
+	return (-1);
+}
