800a507c7c31536edac6b00b46352454e2e02616
prosper <mprosp3r@gmail.com>
2023-05-10 07:20:34 -0700

    correction

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,7 +8,7 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -16,7 +16,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (1);
 	while (text_content[len] != '\0')
 		len++;
-	fd = open(filename, O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 	write(fd, text_content, len);
