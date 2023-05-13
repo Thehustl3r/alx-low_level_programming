d02c8163e6f630c102178a0b0963a0eff3a768b5
Prosper Mugisha <mprosp3r@gmail.com>
2023-05-13 06:24:41 -0700

    add

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Entry program
@@ -8,11 +9,19 @@
  */
 int main (int argc, char **argv)
 {
+	char *input = (char *)argv[1];
+	char comd[100];
+	int flag;
+
 	if (argc != 2)
 	{
 		perror("Usage: elf_header elf_filename\n");
 		return (0);
 	}
-	printf("%s\n", argv[1]);
+
+	comd = "readelf -h " + *input + "| awk 'NR == 1 || NR == 2 || NR || NR == 3 || NR == 4 || NR == 5 || NR == 6 || NR == 7 || NR == 8 || NR == 10'";
+	flag = system(comd);
+	if (flag == -1)
+		return (-1);
 	return (0);
 }
