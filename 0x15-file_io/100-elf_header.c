00fe55019f0a317a735b45b008a3d276d9b6fa2d
Prosper Mugisha <mprosp3r@gmail.com>
2023-05-13 10:30:08 -0700

    correction

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
 /**
  * main - Entry program
  * @argc: the number of argument
@@ -10,8 +12,10 @@
 int main (int argc, char **argv)
 {
 	char *input = (char *)argv[1];
-	char comd[100];
-	int flag;
+	char *cmmd;
+	off_t file_size;
+	char buffer[] = " | awk 'NR==1 || NR==2 || NR==3 || NR==4 || NR==5 || NR==6 || NR==7 || NR==8 || NR==10'";
+	int tmp_fd, fdr;
 
 	if (argc != 2)
 	{
@@ -19,9 +23,36 @@ int main (int argc, char **argv)
 		return (0);
 	}
 
-	comd = "readelf -h " + *input + "| awk 'NR == 1 || NR == 2 || NR || NR == 3 || NR == 4 || NR == 5 || NR == 6 || NR == 7 || NR == 8 || NR == 10'";
-	flag = system(comd);
-	if (flag == -1)
-		return (-1);
+	tmp_fd = open("temp", O_WRONLY | O_CREAT | O_APPEND, 0666);
+	if (tmp_fd == -1)
+		return (1);
+	write(tmp_fd, "readelf -h ", 11);
+	write(tmp_fd, input, sizeof(input));
+	write(tmp_fd, buffer, sizeof(buffer));
+	close(tmp_fd);
+
+	tmp_fd = open("temp", O_RDONLY);
+	if (tmp_fd == -1)
+		return (1);
+
+	file_size = lseek(tmp_fd, 0, SEEK_SET);
+
+	cmmd = malloc(file_size + 1);
+	if (cmmd == NULL)
+	{
+		close(tmp_fd);
+		return (1);
+	}
+	fdr = read(tmp_fd, cmmd, file_size);
+	if (fdr == -1)
+	{
+		close(tmp_fd);
+		free(cmmd);
+	}
+	cmmd[fdr] = '\0';
+	close(tmp_fd);
+	system(cmmd);
+	free(cmmd);
+	system("rm temp");
 	return (0);
 }
