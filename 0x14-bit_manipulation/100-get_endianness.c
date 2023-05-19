09d34c18818d42da6aa1369bd4c5b7beb4406f1a
Prosper Mugisha <mprosp3r@gmail.com>
2023-05-19 11:59:14 -0700

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * get_endianness - the functions finds endiansian
+ *
+ * Return: 0 Always 1
+ */
+int get_endianness(void)
+{
+	unsigned int n = 2;
+	char *ch = (char *) & n;
+	if (*ch)
+		return (1);
+	return (0);
+}
