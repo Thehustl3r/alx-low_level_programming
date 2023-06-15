2e29e83af4f458339d917bc2c85e66cc8abb3e1f
prosper <mprosp3r@gmail.com>
2023-06-15 15:07:20 -0700

    1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * dlistint_len - the function that print the list of double list
+ * @h: pointer to the header
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h);
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
