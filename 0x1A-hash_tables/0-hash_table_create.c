28830d81cf654ce2c286e68dcd95bcee95a02275
prosper <mprosp3r@gmail.com>
2023-07-11 16:00:06 -0700

    update on my codes

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,10 +6,23 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned int i = 0;
 	hash_table_t *hash_table;
 
 	hash_table = malloc(size * sizeof(hash_table_t));
 	if (hash_table == NULL)
 		return (NULL);
+	hash_table->size = size;
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	
+	if (hash_table->array == NULL)
+	{
+		free(hash_table);
+		return (NULL);
+	}
+	for (; i < size; i++)
+	{
+		hash_table->array[i] = NULL;
+	}
 	return (hash_table);
 }
