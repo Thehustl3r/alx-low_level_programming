63da1419376ebbc25b2c69b48691b047af4626c9
prosper <mprosp3r@gmail.com>
2023-07-11 16:32:18 -0700

    1-djb2.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -22,7 +22,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 	for (; i < size; i++)
 	{
-		(hash_table->array)[i] = NULL;
+		hash_table->array[i] = NULL;
 	}
 	return (hash_table);
 }
