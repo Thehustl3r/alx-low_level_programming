4f4fe0793f65b4c3c458852910632f71e3f1eae6
prosper <mprosp3r@gmail.com>
2023-07-15 16:32:00 -0700

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,9 +7,9 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned int i = 0;
-	hash_table_t *hash_table;
+	hash_table_t *hash_table = NULL;
 
-	hash_table = malloc(size * sizeof(hash_table_t));
+	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 		return (NULL);
 	hash_table->size = size;
--- 0x1A-hash_tables/0-main.c
+++ 0x1A-hash_tables/0-main.c
@@ -1,18 +1,21 @@
 #include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "hash_tables.h"
 
 /**
- *  * main - check the code for
+ *  * main - check the code for Holberton School students.
  *   *
  *    * Return: Always EXIT_SUCCESS.
  *     */
 int main(void)
 {
-	    hash_table_t *ht;
+		hash_table_t *ht;
+			size_t i;
 
-	        ht = hash_table_create(1024);
-		    printf("%p\n", (void *)ht);
-		        return (EXIT_SUCCESS);
+				ht = hash_table_create(2048);
+					if (!ht || ht->size != 2048 || !ht->array)
+								return (EXIT_FAILURE);
+						/*Loop through the array, should not SEGV*/
+						for (i = 0; i < 2048 && ht->array[i]; ++i)
+							continue;
+							return (EXIT_SUCCESS);
 }
