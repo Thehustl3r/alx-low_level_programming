8167bc7ca7d2377d86361b359700284cc0525549
prosper <mprosp3r@gmail.com>
2023-07-15 16:53:44 -0700

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,8 +10,8 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	int index;
-	hash_node_t *new_node;
-	char *key_copy, *value_copy;
+	hash_node_t *new_node = NULL, tmp_node = NULL;
+	char *key_copy = NULL, *value_copy = NULL;
 
 	if (key == NULL || ht == NULL || strcmp(key, "") == 0)
 		return (0);
@@ -21,16 +21,29 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	key_copy = strdup(key);
 	value_copy = strdup(value);
-	if (!key_copy || (value && !value_copy))
-	{
-		free(new_node);
-		free(key_copy);
-		free(value_copy);
-	}
 
 	new_node->key = key_copy;
 	new_node->value = value_copy;
-	new_node->next = ht->array[index];
+	new_node->next = NULL;
+	if (ht->array[index])
+	{
+		tmp_node = ht->array[index];
+		while (tmp_node)
+		{
+			if (strcmp(tmp_node->key, key_copy) == 0)
+			{
+				free(ht->array[index]->value);
+				ht->array[index]->value = value_copy;
+				free(key_copy);
+				free(new_node);
+				return (1);
+			}
+			tmp_node = tmp_node->next;
+		}
+		tmp_node = ht->array[index];
+		new_node->next = tmp_node;
+		(ht->array)[index] = new_node;
+	}
 	ht->array[index] = new_node;
 	return (1);
 }
