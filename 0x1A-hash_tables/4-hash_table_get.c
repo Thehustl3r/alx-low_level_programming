0525a0770ba1b5553a431a1079949f35461083e9
prosper <mprosp3r@gmail.com>
2023-07-15 18:02:51 -0700

    5-hash_table_print.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -14,7 +14,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if (ht == NULL || key == NULL)
 		return (NULL);
 
-	index = key_index((const unsigned char*)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 	if ((ht->array)[index] == NULL)
 		return (NULL);
 	value = (ht->array)[index]->value;
---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function that prints the hash table
+ * @ht: hash table
+ * Return: Nothing.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node = NULL;
+	int first_pair = 1;
+	unsigned long int i = 0;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			if (!first_pair)
+				printf(", ");
+
+			printf("'%s': '%s'", node->key, node->value);
+			first_pair = 0;
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
