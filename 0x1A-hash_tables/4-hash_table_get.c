bcd552c80add72d8ccebfef6d7d9a3bc5294697f
prosper <mprosp3r@gmail.com>
2023-07-15 17:22:31 -0700

    4-hash_table_get.c

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - function to look for value in table
+ * @ht: hash table
+ * @key: key to search
+ * Return: the value
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	char *value = NULL;
+	unsigned int index;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
+	index = key_index((const unsigned char*)key, ht->size);
+	if ((ht->array)[index] == NULL)
+		return (NULL);
+	value = (ht->array)[index]->value;
+	return (value);
+}
