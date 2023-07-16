99dc77876d91e232a323ea973fa060829bf2391d
prosper <mprosp3r@gmail.com>
2023-07-15 18:07:24 -0700

    5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -14,7 +14,7 @@ void hash_table_print(const hash_table_t *ht)
 	if (ht == NULL)
 		return;
 	printf("{");
-	for (; i < ht->size; i++)
+	for (; i < ht->size - 1; i++)
 	{
 		node = ht->array[i];
 		while (node)
